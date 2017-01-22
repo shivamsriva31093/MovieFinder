0b8352a023e906065b33713e1076d53b815d9713
Shashank <shashank.mohan82@gmail.com>
2017-01-23 00:00:05 +0530

    Initial commit

---
+++ app/src/main/java/task/application/com/moviefinder/view/SearchListActivity.java
@@ -0,0 +1,15 @@
+package task.application.com.moviefinder.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import task.application.com.moviefinder.R;
+
+public class SearchListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_list);
+    }
+}
