d4fc81c99e0cb018fd44b745b4480d459610c0df
Shivam <shivam.srivastava31093@gmail.com>
2017-02-06 20:53:56 +0530

    basic fragment creation

--- app/src/main/java/task/application/com/moviefinder/ui/searchlist/SearchListFragment.java
+++ app/src/main/java/task/application/com/moviefinder/ui/searchlist/SearchListFragment.java
@@ -1,8 +1,28 @@
 package task.application.com.moviefinder.ui.searchlist;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import task.application.com.moviefinder.R;
+
 /**
  * Created by sHIVAM on 2/6/2017.
  */
 
-public class SearchListFragment {
+public class SearchListFragment extends Fragment {
+
+    public static SearchListFragment newInstance() {
+        return new SearchListFragment();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_searchlist, container, false);
+        return rootView;
+    }
 }
