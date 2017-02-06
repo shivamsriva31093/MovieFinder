caceb3604d1a332e1e797bc209ffa9a20221e703
Shivam <shivam.srivastava31093@gmail.com>
2017-02-06 20:53:08 +0530

    Updated the codebase using MVP architecture. added view fragment

--- app/src/main/java/task/application/com/moviefinder/ui/searchlist/SearchListActivity.java
+++ app/src/main/java/task/application/com/moviefinder/ui/searchlist/SearchListActivity.java
@@ -1,10 +1,36 @@
 package task.application.com.moviefinder.ui.searchlist;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.RelativeLayout;
+
+import task.application.com.moviefinder.R;
+import task.application.com.moviefinder.util.Util;
 
 /**
  * Created by sHIVAM on 2/6/2017.
  */
 
 public class SearchListActivity extends AppCompatActivity {
+
+    private static final String TAG = SearchListActivity.class.getName();
+    private static final String LIST_FRAG_TAG = "searchlist_frag";
+    private RelativeLayout fragment_container;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_list);
+
+        Fragment fragment = (SearchListFragment)
+                getSupportFragmentManager().findFragmentByTag(LIST_FRAG_TAG);
+        if(fragment == null) {
+            fragment = SearchListFragment.newInstance();
+            Util.addFragmentToActivity(getSupportFragmentManager(), fragment,
+                    R.id.activity_search_list, LIST_FRAG_TAG);
+        }
+
+    }
 }
