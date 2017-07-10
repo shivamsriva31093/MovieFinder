db5c3a0c99607816e6df181d8e8103ec6ee68206
Shivam <shivam.srivastava31093@gmail.com>
2017-07-10 16:34:26 +0530

    advanced movie query from api

--- androidtmdbwrapper/src/main/java/com/androidtmdbwrapper/apiservices/MoviesService.java
+++ androidtmdbwrapper/src/main/java/com/androidtmdbwrapper/apiservices/MoviesService.java
@@ -1,6 +1,8 @@
 package com.androidtmdbwrapper.apiservices;
 
 import com.androidtmdbwrapper.model.core.AppendToResponse;
+import com.androidtmdbwrapper.model.core.SearchResult;
+import com.androidtmdbwrapper.model.movies.BasicMovieInfo;
 import com.androidtmdbwrapper.model.movies.MovieInfo;
 
 import io.reactivex.Observable;
@@ -19,4 +21,33 @@ public interface MoviesService {
             @Path("id") int tmdbId,
             @Query("append_to_response") AppendToResponse appendToResponse
     );
+
+    @GET("movie/now_playing")
+    Observable<SearchResult<BasicMovieInfo>> getNowPlaying(
+            @Query("language") String ISO639_1_language,
+            @Query("page") int page,
+            @Query("region") String ISO3166_1_region
+    );
+
+    @GET("movie/popular")
+    Observable<SearchResult<BasicMovieInfo>> getPopular(
+            @Query("language") String ISO639_1_language,
+            @Query("page") int page,
+            @Query("region") String ISO3166_1_region
+    );
+
+    @GET("movie/top_rated")
+    Observable<SearchResult<BasicMovieInfo>> getTopRated(
+            @Query("language") String ISO639_1_language,
+            @Query("page") int page,
+            @Query("region") String ISO3166_1_region
+    );
+
+    @GET("movie/upcoming")
+    Observable<SearchResult<BasicMovieInfo>> getUpcoming(
+            @Query("language") String ISO639_1_language,
+            @Query("page") int page,
+            @Query("region") String ISO3166_1_region
+    );
+
 }
